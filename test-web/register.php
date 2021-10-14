7b2afb4afc5a367254f331e992064fead0bffbf4
vs256 <44126680+vs256@users.noreply.github.com>
2021-10-14 16:04:21 -0700

    Test

--- test-web/register.php
+++ test-web/register.php
@@ -16,8 +16,8 @@
         }
         else
         {
-            $hash = password_hash($password, PASSWORD_BCRYPT);
-            $con->query(query: "INSERT INTO users(name,email,password) VALUES ('$name','$email','$hash')");
+            //$hash = password_hash($password, PASSWORD_BCRYPT);
+            $con->query(query: "INSERT INTO users(name,email,password) VALUES ('$name','$email','$password')");
             $msg = "You have been registered!";
 
         }
