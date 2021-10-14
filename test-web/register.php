2cef571cba9ad9fa36beb7720a4a4b7b37d6d8fc
vs256 <44126680+vs256@users.noreply.github.com>
2021-10-14 15:53:46 -0700

    Test

--- test-web/register.php
+++ test-web/register.php
@@ -3,25 +3,7 @@
 
     
     if(isset($_POST['submit'])){
-        $con = new mysqli('localhost', 'root', 'computersecurity', 'passwordHashing');
-
-        $name = $con->real_escape_string($_POST['name']);
-        $email = $con->real_escape_string($_POST['email']);
-        $password = $con->real_escape_string($_POST['password']);
-        $cPassword = $con->real_escape_string($_POST['cPassword']);
         
-
-        if ($password != $cPassword)
-        {
-            $msg = "Please Check Your Passwords!";
-        }
-        else
-        {
-            $hash = password_hash($password, algo: PASSWORD_BCRYPT);
-            $con->query(query: "INSERT INTO users (name, email, password) VALUES ('$name','$email','$hash')");
-            $msg = "You have been registered!";
-
-        }
     }
 ?>
 
