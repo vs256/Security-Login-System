e1bec07e3ab0c4e0c3c984d546a6b8dc2dd3fc62
vs256 <44126680+vs256@users.noreply.github.com>
2021-10-14 16:34:18 -0700

    Test

--- test-web/register.php
+++ test-web/register.php
@@ -2,7 +2,7 @@
     $msg = "";
 
     if(isset($_POST['submit'])){
-        $con = new mysqli('localhost', 'root', 'computersecurity');
+        $con = new mysqli('localhost', 'root', '');
 
         $name = $con->real_escape_string($_POST['name']);
         $email = $con->real_escape_string($_POST['email']);
