55d08fdda68a7f83ab4a2a8d2276e158c6281c21
vs256 <44126680+vs256@users.noreply.github.com>
2021-10-14 14:16:09 -0700

    Changed installation shell script

--- installation.sh
+++ installation.sh
@@ -1,43 +1,35 @@
-#ubuntu 18.04
-sudo apt update #Updating the cache
-Y
-sudo apt full-upgrade #Updating the cache
-Y
-
-
-sudo apt install apache2  #Install apache2
-Y
-
-sudo apt install ufw #Install UFW - firewall
-sudo ufw allow OpenSSH #Allow OpenSSH to be able to SSH to port 22
-sudo ufw allow "Apache Full" #Allow Apache Full http traffic access to ports 80 & 443
-sudo ufw enable #enable UFW
-
-sudo apt install mysql-server #install mysql server
-Y
-sudo mysql_secure_installation #secure mysql connection
-no #decline to use validate password plugin
-compsecurity #password
-compsecurity #confirm password
-y #yes to removing anonymous users 
-y #yes to disallow root login remotely
-y #remove test database and access to it
-y #reload privilege tables
-systemctl status mysql.service #start mysql incase it is not running
-
-sudo mysql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'compsecurity';  #change root password
-FLUSH PRIVILEGES; #reload the grant tables and put changes into effect
-exit
-
-sudo apt install php libapache2-mod-php php-mysql #installs PHP
-Y
-sudo systemctl restart apache2 #restart server to save changes
-
-sudo chown -R $USER /var/www #add permissions to /var/www/
-
-
-cd /var/www/html #this is where the website files have to be to run it
-git clone https://github.com/vs256/Security-Design-Website.git #clone our git
-sudo mv /var/www/html/Security-Design-Website/web/* /var/www/html #move all the files of our website in our git repo to /var/www/html
-
+ 
+ # Ask value for mysql root password   
+ read -p 'db_root_password [secretpasswd]: ' db_root_password  
+ echo  
+   
+ # Update system  
+ sudo apt-get update -y  
+   
+ ## Install APache  
+ sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y  
+   
+ ## Install PHP  
+ apt-get install php libapache2-mod-php php-mysql -y  
+   
+ # Install MySQL database server  
+ export DEBIAN_FRONTEND="noninteractive"  
+ debconf-set-selections <<< "mysql-server mysql-server/root_password password $db_root_password"  
+ debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $db_root_password"  
+ apt-get install mysql-server -y  
+   
+ # Enabling Mod Rewrite  
+ sudo a2enmod rewrite  
+ sudo php5enmod mcrypt  
+   
+ ## Install PhpMyAdmin  
+ sudo apt-get install phpmyadmin -y  
+   
+ ## Configure PhpMyAdmin  
+ echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf  
+   
+ # Set Permissions  
+ sudo chown -R www-data:www-data /var/www  
+   
+ # Restart Apache  
+ sudo service apache2 restart  
