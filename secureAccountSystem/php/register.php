fbd93481666dff817890179ea9b8310c199a5774
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-02 00:19:18 -0700

    Test

--- secureAccountSystem/php/register.php
+++ secureAccountSystem/php/register.php
@@ -1,7 +1,7 @@
 <?php
-    require_once 'utils.php';
+	require_once 'sendValidationEmail.php';
+	$errors = [];
 
-    $errors = [];
 
 	if(!isset($_POST['name']) || strlen($_POST['name']) > 255 || !preg_match('/^[a-zA-Z- ]+$/', $_POST['name'])) {
 		$errors[] = 1;
@@ -20,41 +20,48 @@
 	}
 
 
-    if(count($errors) === 0) {
-        //Connect to database
-        $C = connect();
 
-        if($C) 
-        {
-            //Check if user with same email already exists
-            $res = sqlSelect($C, 'SELECT id FROM users where email=?', 's', $_POST['email']);
-            if($res && $res->num_rows === 0)
-            {
-                //Actualy create the account
-                $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                $id = sqlInsert($C, 'INSERT INTO users VALUES (NULL, ?, ?, ?, 0)', 'sss', $_POST['name'], $_POST['email'], $_POST['hash']);
-                if($id !== -1) {
-                    //$err = sendValidationEmail($_POST['email']);
-                    if($err === 0) {
-                        $errors[] = 0;
-                    }
-                    else {
-                        $errors[] = $err + 9;
-                    }
-                }
-                else {
-                    //Failed to insert into database
-                    $errors[] = 6;
-                }
-                $res->free_result();
-            }
-            else {
-                //This email is already in use
-            }
-        }
-        else
-        {
-            //Failed to connect to database
-        }
+	if(count($errors) === 0) {
+		if(isset($_POST['csrf_token']) && validateToken($_POST['csrf_token'])) {
+			//Connect to database
+			$C = connect();
+			if($C) {
+				//Check if user with same email already exists
+				$res = sqlSelect($C, 'SELECT id FROM users WHERE email=?', 's', $_POST['email']);
+				if($res && $res->num_rows === 0) {
+					//Actually create the account
+					$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+					$id = sqlInsert($C, 'INSERT INTO users VALUES (NULL, ?, ?, ?, 0)', 'sss', $_POST['name'], $_POST['email'], $hash);
+					if($id !== -1) {
+						$err = sendValidationEmail($_POST['email']);
+						if($err === 0) {
+							$errors[] = 0;
+						}
+						else {
+							$errors[] = $err + 9;
+						}
+					}
+					else {
+						//Failed to insert into database
+						$errors[] = 6;
+					}
+					$res->free_result();
+				}
+				else {
+					//This email is already in use
+					$errors[] = 7;
+				}
+			}
+			else {
+				//Failed to connect to database
+				$errors[] = 8;
+			}
+		}
+		else {
+			//Invalid CSRF Token
+			$errors[] = 9;
+		}
+	}
 
-    }
+
+	echo json_encode($errors);
