b4341d647e9c920a9b652448c2bc1448aab84528
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-02 00:58:29 -0700

    Test

--- secureAccountSystem/php/register.php
+++ secureAccountSystem/php/register.php
@@ -23,6 +23,7 @@
 
 	if(count($errors) === 0) {
 		if(isset($_POST['csrf_token']) && validateToken($_POST['csrf_token'])) {
+			//Connect to database
 			$C = connect();
 			if($C) {
 				//Check if user with same email already exists
@@ -32,7 +33,7 @@
 					$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 					$id = sqlInsert($C, 'INSERT INTO users VALUES (NULL, ?, ?, ?, 0)', 'sss', $_POST['name'], $_POST['email'], $hash);
 					if($id !== -1) {
-						//$err = sendValidationEmail($_POST['email']);
+						$err = sendValidationEmail($_POST['email']);
 						if($err === 0) {
 							$errors[] = 0;
 						}
@@ -57,11 +58,10 @@
 			}
 		}
 		else {
-			//Invalid csrf token sent
+			//Invalid CSRF Token
 			$errors[] = 9;
 		}
 	}
-		
-	
+
 
 	echo json_encode($errors);
