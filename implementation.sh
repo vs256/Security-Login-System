84525c911ac2cf4493e906ac2139842f9948c766
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-03 19:47:40 -0700

    Test

--- implementation.sh
+++ implementation.sh
@@ -28,6 +28,7 @@ create table users(
    email VARCHAR(50) NOT NULL,
    password VARCHAR(255) NOT NULL,
    verified INT NOT NULL DEFAULT '0',
+   loginCount INT DEFAULT '0',
    PRIMARY KEY ( id )
 );"
 
--- secureAccountSystem/register.php
+++ secureAccountSystem/register.php
@@ -14,6 +14,7 @@
 <body>
 	
 	<div class="formWrapper">
+		<div class="navigation">
 		<form id="registerForm">
 			<h1>Register</h1>
 			<div id="errs" class="errcontainer"></div>
@@ -27,8 +28,15 @@
 			</div>
 			<div class="inputblock">
 				<label for="password">Password</label>
-				<input id="password" name="password" type="password" autocomplete="new-password" placeholder="Enter your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
-			</div>
+				<input class="password" id="password" name="password" type="password" autocomplete="new-password" placeholder="Enter your password" onkeyup="triggerIndicator()" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                <span class ="showBtn">SHOW</span>
+            </div>
+            <div class="indicator">
+                <span class="weak"></span>
+                <span class="medium"></span>
+                <span class="strong"></span>
+            </div>
+            <div class="text">Your password is too weak.</div>
 			<div class="inputblock">
 				<label for="confirm-password">Confirm Password</label>
 				<input id="confirm-password" name="confirm-password" type="password" autocomplete="new-password" placeholder="Confirm your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
@@ -42,9 +50,13 @@
 		</form>
 		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
 		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
+		</div>
 	</div>
 
 
-	<script src="<?php echo dirname($_SERVER['SERVER_NAME']) . '/script.js' ?>"></script>
+	<script src="<?php echo dirname($_SERVER['SERVER_NAME']) . '/script.js' ?>">
+        
+    </script>
+
 </body>
 </html>
