f6fda38e87d68f036d74e5763884428786da3ddd
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-03 21:21:43 -0700

    Test

--- secureAccountSystem/script.js
+++ secureAccountSystem/script.js
@@ -240,7 +240,6 @@ function triggerIndicator() {
 	const text = document.querySelector(".text");
 	const showBtn = document.querySelector(".showBtn");
 			
-	const no = -1;
 			const password = document.querySelector(".password");
 	
 			let regExpWeak = /[a-z]/;
