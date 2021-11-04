edf4c279e3a1b54b11d6303163e471450f0f1db0
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-03 17:18:19 -0700

    Test

--- secureAccountSystem/login.php
+++ secureAccountSystem/login.php
@@ -8,34 +8,34 @@
 	<meta name="csrf_token" content="<?php echo createToken(); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login - Secure Site</title>
-	<link rel="stylesheet" href="<?php echo dirname($_SERVER['SERVER_NAME']) . '/clothingBusiness.css' ?>" />
+	<link rel="stylesheet" href="<?php echo dirname($_SERVER['SERVER_NAME']) . '/style.css' ?>" />
 </head>
 <body>
-<div class="container">
-        <div class="background">
-            <div class="inputBox">
-                <a href="./index.php">
-                    <img class="thread" src="./php/images/LoginAvatar.png" alt="Thread and Needle" >
-                </a>
-                <h1>
-                    Login
-                </h1>
-                    <form id="loginForm" >
-                        <div id="errs" class="errorcontainer"></div>
-                        <label for="email">Email</label>
-				        <input id="email" name="email" type="email" autocomplete="email" required placeholder="Enter your Email" onkeydown="if(event.key === 'Enter'){event.preventDefault();login();}"/>   
-                        <label for="password">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required placeholder="Enter Your Password" onkeydown="if(event.key === 'Enter'){event.preventDefault();login();}"/>
-                        <input type="submit" onclick="login();" name="submit" placeholder="Login">
-                        <br>
-                        <a href="./resetpassword"> Forgot Password?</a>
-                        <br>
-                        <a href="./register">New User? Create Account Here</a>
-                    </form>
-            </div>
-        </div>
-        
-    </div>
+	<div class="formWrapper">
+		<form id="loginForm">
+			<h1>Log In</h1>
+			<div id="errs" class="errorcontainer"></div>
+			<div class="inputblock">
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" autocomplete="email" required placeholder="Enter your email" onkeydown="if(event.key === 'Enter'){event.preventDefault();login();}"/>
+			</div>
+			<div class="inputblock">
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" autocomplete="current-password" required placeholder="Enter your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();login();}"/>
+			</div>
+			<br>
+			<div class="btn" onclick="login();">Log In</div>
+			<br>
+			<br>
+			<br>
+			<a href="./register">Dont have an account? Sign Up</a>
+			<br>
+			<a href="./resetpassword">Forgot your password? Reset it</a>
+		</form>
+
+		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
+		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
+	</div>
 
 	<script src="<?php echo dirname($_SERVER['SERVER_NAME']) . '/script.js' ?>"></script>
 </body>
