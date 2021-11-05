36eb30950ca4f0046ff4ca1fd3e6d49287af9c89
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-04 17:44:41 -0700

    Test

--- secureAccountSystem/register.php
+++ secureAccountSystem/register.php
@@ -1,69 +1,67 @@
-<?php 
-	require_once 'php/utils.php';
+<?php
+require_once 'php/utils.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	
-	<meta name="csrf_token" content="<?php echo createToken(); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Register - Secure Site</title>
-	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-	<link rel="stylesheet" href="<?php echo dirname($_SERVER['SERVER_NAME']) . '/style.css' ?>">
+    <meta charset="UTF-8">
+    <meta name="csrf_token" content="<?php echo createToken(); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Secure Site</title>
+    <link rel="stylesheet" href="<?php echo dirname($_SERVER['SERVER_NAME']) . '/clothingBusiness.css' ?>" />
+
 </head>
+
 <body>
-	
-	<div class="formWrapper">
-		<div class="navigation">
-		<form id="registerForm">
-			<h1>Register</h1>
-			<div id="errs" class="errcontainer"></div>
-			<div class="inputblock">
-				<label for="name">Name</label>
-				<input id="name" name="name" type="text" autocomplete="name" placeholder="Enter your name" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
-			</div>
-			<div class="inputblock">
-				<label for="email">Email</label>
-				<input id="email" name="email" type="email" autocomplete="email" placeholder="Enter your email" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
-			</div>
-			<div class="inputblock">
-				<label for="password">Password</label>
-				<input class="password" id="password" name="password" type="password" autocomplete="new-password" placeholder="Enter your password" onkeyup="triggerIndicator()" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
-                <span class ="showBtn">SHOW</span>
+
+    <div class="container">
+        <div class="background">
+            <div class="createBox">
+                <a href="./login.php">
+                    <img class="thread" src="./php/images/loginAvatar.png" alt="Thread and Needle">
+                </a>
+                <h2>
+                    Create Account
+                </h2>
+                <form>
+                    <label for="name">Name</label>
+                    <input id="name" name="name" type="text" autocomplete="name" placeholder="Enter Your Name" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                    <label for="dob">Date of Birth</label>
+                    <input id="dob" name="dob" type="text" autocomplete="bday" placeholder="MM/DD/YYYY" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                    <label for="email">Email</label>
+                    <input id="email" name="email" type="email" autocomplete="email" placeholder="Enter Your Email" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                    <label for="password">Password</label>
+                    <input id="password" name="password" type="password" autocomplete="new-password" placeholder="Enter Your Password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                    <label for="confirm-password">Confirm Password</label>
+                    <input id="confirm-password" name="confirm-password" type="password" autocomplete="new-password" placeholder="Confirm Your Password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
+                    <!--Select is a drop down menu.-->
+                    <label>Please Pick A Security Question To Answer.</label>
+                    <select name="languages" id="">
+                        <option value="question1">What Street Did You Grow Up On?</option>
+                        <option value="question2">What Was The Name Of Your First Pet?</option>
+                        <option value="question3">What Elementary School Did You Attend?</option>
+                        <option value="question4">What Was The Make And Model Of Your First Car?</option>
+                        <option value="question5">Where Did Your Parents Meet?</option>
+                        <option value="question6">?</option>
+
+                    </select>
+                    <input id="answer" type="text" name="answer" autocapitalize="characters" required placeholder="Enter Your answer" onkeydown="if(event.key === 'Enter'){event.preventDefault();login();}">
+                    <!-- <button type="submit"> Submit</button> -->
+                    <input type="submit" onclick="register();" name="submit" placeholder="Register">
+                    <br>
+                    <a href="./resetpassword"> Forgot Password?</a>
+                    <br>
+                    <a href="./login">Already have an account? Log In</a>
+                    <br>
+                </form>
             </div>
-            <div class="indicator">
-                <span class="weak"></span>
-                <span class="medium"></span>
-                <span class="strong"></span>
-            </div>
-            <div class="text">Your password is too weak.</div>
-			<div class="inputblock">
-				<label for="confirm-password">Confirm Password</label>
-				<input id="confirm-password" name="confirm-password" type="password" autocomplete="new-password" placeholder="Confirm your password" onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" />
-			</div>
+        </div>
 
-			<form action="response.php" method="post">
-			<br />
-			<div class="g-recaptcha" data-sitekey="6LdoQxMdAAAAAI1iC2QqRB41Hxika4ohpJa3k5p3"></div>
-			
-
-			<br>
-			<div class="btn" onclick="register();" type="submit">Sign Up</div>
-			</form>
-			
-			<br>
-			<a href="./login">Already have an account? Log In</a>
-		</form>
-		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
-		<svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 32 1440 320"><defs><linearGradient id="a" x1="50%" x2="50%" y1="-10.959%" y2="100%"><stop stop-color="#ffffff" stop-opacity=".10" offset="0%"/><stop stop-color="#FFFFFF" stop-opacity=".05" offset="100%"/></linearGradient></defs><path fill="url(#a)" fill-opacity="1" d="M 0 320 L 48 288 C 96 256 192 192 288 160 C 384 128 480 128 576 112 C 672 96 768 64 864 48 C 960 32 1056 32 1152 32 C 1248 32 1344 32 1392 32 L 1440 32 L 1440 2000 L 1392 2000 C 1344 2000 1248 2000 1152 2000 C 1056 2000 960 2000 864 2000 C 768 2000 672 2000 576 2000 C 480 2000 384 2000 288 2000 C 192 2000 96 2000 48 2000 L 0 2000 Z"></path></svg>
-		</div>
-	</div>
+    </div>
 
 
-	<script src="<?php echo dirname($_SERVER['SERVER_NAME']) . '/script.js' ?>">
-        
-    </script>
-
+    <script src="<?php echo dirname($_SERVER['SERVER_NAME']) . '/script.js' ?>"></script>
 </body>
+
 </html>
